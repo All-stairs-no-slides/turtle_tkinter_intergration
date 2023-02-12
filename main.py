93d05d417089baf0183b9a62be5574b2b793b4e5
Alp-Gaming <amcd13@outlook.com>
2023-02-13 06:19:46 +1000

    made colours colourful

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def drawing(type_of_action=[], colours=[], cursor_widths=[],  circle_radii=[], d
     pendown()
     for each_move in range(len(type_of_action)):
         print(each_move)
-        # changing general/unspecified things
+        # changing general/unspecified things colours_list[each_move][0], colours_list[each_move][1], colours_list[each_move][2]
         pensize(cursor_widths[each_move])
-        color("black")
+        pencolor(colours[each_move][0], colours[each_move][1], colours[each_move][2])
         # checking for each type of line and drawing it
         if type_of_action[each_move] == "line":
             forward(distance_of_moves[line_num])
@@ -60,6 +60,7 @@ def front_end():
 
 def draw():
     drawing(move_type_list, colours_list, cursor_widths_list, circle_radii_list, distance_of_moves_list, angle_of_turns_list)
+    move_type_list, colours_list, cursor_widths_list, circle_radii_list, distance_of_moves_list, angle_of_turns_list = []
 
 def create_your_pattern():
     print("create your pattern")
@@ -68,11 +69,14 @@ def create_your_pattern():
 def single_line_feature_creation():
     # create the features of each line
     print("customise your lines")
-    global move_type, move_type_value, move_type_width
+    global move_type, move_type_value, move_type_width, red, green, blue
     # general variables
     move_type = IntVar()
     move_type_value = IntVar()
     move_type_width = IntVar()
+    green = IntVar()
+    red = IntVar()
+    blue = IntVar()
     line_input = Toplevel()
 
     # general title
@@ -103,6 +107,18 @@ def single_line_feature_creation():
     # width spinbox
     width_value = Spinbox(values, from_=0, to=10000, font=spinbox_font, textvariable=move_type_width)
 
+    #colour
+    colours_label = Label(values, text = "choose line colour (RGB)", font= subtitle_font)
+    #red
+    red_label = Label(values, text = "red", font= subtitle_font)
+    red_scale = Scale(values, from_=0, to=255, orient=HORIZONTAL, variable=red)
+    # green
+    green_label = Label(values, text = "green", font= subtitle_font)
+    green_scale = Scale(values, from_=0, to=255, orient=HORIZONTAL, variable=green)
+    # blue
+    blue_label = Label(values, text = "blue", font= subtitle_font)
+    blue_scale = Scale(values, from_=0, to=255, orient=HORIZONTAL, variable=blue)
+
     # dedicate values
 
     # function for button
@@ -121,18 +137,30 @@ def single_line_feature_creation():
     # value spinbox implemented
     choose_amount_of_movement.grid(row=1, column=1)
     # final dedication button implemented
-    dedicate_values.grid(row=5, column=1)
+    dedicate_values.grid(row=12, column=1)
     # actual spinbox implementations now
     line_value.grid(row=2, column=1)
     # width label implementation
     choose_width.grid(row=3, column=1)
     # width spinbox implementation
     width_value.grid(row=4, column=1)
+
+    # colours implementation
+    colours_label.grid(row=5, column=1)
+    red_label.grid(row=6, column=1)
+    red_scale.grid(row=7, column=1)
+    green_label.grid(row=8, column=1)
+    green_scale.grid(row=9, column=1)
+    blue_label.grid(row=10, column=1)
+    blue_scale.grid(row=11, column=1)
+
     line_input.mainloop()
        
        
 def find_where_to_append():
     cursor_widths_list.append(move_type_width.get())
+    colours_list.append([red.get(), green.get(), blue.get()])
+    print(colours_list)
     if move_type.get() == 1:
         distance_of_moves_list.append(move_type_value.get())
         return
@@ -159,6 +187,9 @@ def main():
 
 if __name__ == '__main__':
     print_hi('main program')
+    # turtle init
+    turtle = Turtle()
+    turtle.screen.colormode(255)
     # more general variables
     header_font = "Times 30 underline"
     subtitle_font = "Times 16"
